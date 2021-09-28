ee74f2fc2d814fa120cafe45141633b2066d20cc
Thomas <tpienne51@gmail.com>
2021-09-28 14:42:19 +0200

    Ajout du test carte en 45

---
+++ game.py
@@ -0,0 +1,55 @@
+import pygame
+import sys
+
+from world import World
+from settings import TILE_SIZE
+
+class Game:
+    def __init__(self, screen, clock):
+        self.screen = screen
+        self.clock = clock
+        self.width, self.height = self.screen.get_size()
+        self.world = World(10, 10, self.width, self.height)
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(60)
+            self.events()
+            self.update()
+            self.draw()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+
+        for x in range(self.world.grid_length_x):
+            for y in range(self.world.grid_length_y):
+                sq = self.world.world[x][y]["cart_rect"]
+                rect = pygame.Rect(sq[0][0], sq[0][1], TILE_SIZE, TILE_SIZE)
+                pygame.draw.rect(self.screen, (0, 0, 255), rect, 1)
+
+                render_pos = self.world.world[x][y]["render_pos"]
+                self.screen.blit(self.world.tiles["grass"], (render_pos[0] + self.width/2, render_pos[1] + self.height/4))
+
+                tile = self.world.world[x][y]["tile"]
+                if tile != "":
+                    self.screen.blit(self.world.tiles[tile], (render_pos[0] + self.width/2, render_pos[1] + self.height/4 - self.world.tiles[tile].get_height() - TILE_SIZE))
+
+                p = self.world.world[x][y]["iso_poly"]
+                p = [(x + self.width/2, y + self.height/4) for x, y in p]
+                pygame.draw.polygon(self.screen, (255, 0, 0), p, 1)
+
+        pygame.display.flip()
---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from game import Game
+
+
+def main():
+    pygame.init()
+    pygame.mixer.init()
+
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Age of Cheap Empires")
+    icon = pygame.image.load("castle.png")
+    pygame.display.set_icon(icon)
+
+    running = True
+    playing = True
+
+    game = Game(screen, clock)
+
+    while running:
+
+        # start menu
+
+        while playing:
+            # game loop
+            game.run()
+
+
+if __name__ == "__main__":
+    main()
