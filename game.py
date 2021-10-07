8c3b5a2de9aa900199be28bdbba5fb8dfbb19ba6
noemie56140 <noemie56140@hotmail.fr>
2021-10-07 16:33:39 +0200

    Pose objet

--- game.py
+++ game.py
@@ -12,11 +12,12 @@ class Game:
         self.screen = screen
         self.clock = clock
         self.width, self.height = self.screen.get_size()
-        self.world = World(100, 100, self.width, self.height)  # les deux premiers int sont longueur et largeur du monde
+
+        self.hud = Hud(self.width, self.height)
+        self.world = World(self.hud, 100, 100, self.width, self.height)  # les deux premiers int sont longueur et largeur du monde
 
         self.camera = Camera(self.width, self.height)
 
-        self.hud = Hud(self.width,self.height)
 
     def run(self):
         self.playing = True
@@ -45,20 +46,7 @@ class Game:
 
     def draw(self):
         self.screen.fill((0, 0, 0))
-
-        self.screen.blit(self.world.grass_tiles, (self.camera.scroll.x, self.camera.scroll.y))
-
-        for x in range(self.world.grid_length_x):
-            for y in range(self.world.grid_length_y):
-
-                render_pos = self.world.world[x][y]["render_pos"]
-
-                tile = self.world.world[x][y]["tile"]
-                if tile != "":
-                    self.screen.blit(self.world.tiles[tile],
-                                     (render_pos[0] + self.world.grass_tiles.get_width() / 2 + self.camera.scroll.x,
-                                      render_pos[1] - (self.world.tiles[tile].get_height() - TILE_SIZE) + self.camera.scroll.y))
-
+        self.world.draw(self.screen, self.camera)
         self.hud.draw(self.screen)
 
         draw_text(self.screen, 'fps = {}'.format(round(self.clock.get_fps())), 25, (255, 255, 255), (10, 10))
--- world.py
+++ world.py
@@ -5,7 +5,8 @@ from settings import TILE_SIZE
 
 class World:
 
-    def __init__(self, grid_length_x, grid_length_y, width, height):
+    def __init__(self, hud, grid_length_x, grid_length_y, width, height):
+        self.hud = hud
         self.grid_length_x = grid_length_x
         self.grid_length_y = grid_length_y
         self.width = width
@@ -14,6 +15,23 @@ class World:
         self.grass_tiles = pygame.Surface((self.grid_length_x * TILE_SIZE * 2, self.grid_length_y * TILE_SIZE + 2 * TILE_SIZE)).convert_alpha()
         self.tiles = self.load_images()
         self.world = self.create_world()
+
+
+    def update(self):
+        pass
+
+
+    def draw(self,screen, camera):
+        screen.blit(self.grass_tiles, (camera.scroll.x, camera.scroll.y))
+
+        for x in range(self.grid_length_x):
+            for y in range(self.grid_length_y):
+                render_pos = self.world[x][y]["render_pos"]
+                tile = self.world[x][y]["tile"]
+                if tile != "":
+                    screen.blit(self.tiles[tile],
+                                     (render_pos[0] + self.grass_tiles.get_width() / 2 + camera.scroll.x,
+                                      render_pos[1] - (self.tiles[tile].get_height() - TILE_SIZE) + camera.scroll.y))
 
     def create_world(self):
         world = []
