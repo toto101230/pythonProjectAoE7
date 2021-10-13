62a79da70ca6f1e0b59bab7d7ce1aefd5def8c3c
Mathieu PENE <mathieu.pene@gmail.com>
2021-10-13 22:06:44 +0200

    ajout bouton noir hud

--- hud.py
+++ hud.py
@@ -78,18 +78,27 @@ class Hud:
                     self.selected_tile = tile
 
     def draw(self, screen):
-        #
         screen.blit(self.resouces_surface, (0, 0))
         screen.blit(self.build_surface, (self.width * 0.84, self.height * 0.74))
 
         if self.examined_tile is not None:
             w, h = self.select_rect.width, self.select_rect.height
             screen.blit(self.select_surface, (self.width * 0.35, self.height * 0.79))
+
+            #affichage de l'image du batiment avec son nom et son nombre de vie
             img = self.examined_tile.image.copy()
             img_scale = self.scale_image(img, h=h * 0.7)
             screen.blit(img_scale, (self.width * 0.35 + 10, self.height * 0.79 + 40))
-            draw_text(screen, self.examined_tile.name, 40 , (255, 255, 255), self.select_rect.topleft)
-            draw_text(screen, str(self.examined_tile.health), 30 , (255, 255, 255), self.select_rect.center)
+            draw_text(screen, self.examined_tile.name, 60, "#ff0000", self.select_rect.midtop)
+            draw_text(screen,str(self.examined_tile.health), 30, (255, 255, 255), self.select_rect.center)
+
+            #affichage du bouton
+            img2 = pg.image.load("assets/hud/carre_noir.jpg")
+            img2_scale = pg.transform.scale(img2,(60,60))
+            screen.blit(img2_scale, (self.width * 0.35 +299, self.height * 0.79 + 60))
+
+
+
 
 
         for tile in self.tiles:
