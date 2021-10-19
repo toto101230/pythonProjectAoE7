8fafde083c5e8a16175c4c7f8e161c6861d60544
FabienZX <90708495+FabienZX@users.noreply.github.com>
2021-10-19 16:20:21 +0200

    affichage time en haut à gauche mm:ss  bouger dans l'hud si possible

--- time.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class Time:
-    def __init__(self):
-        #60000ms = 1 min
-        self.game_min = (pygame.time.get_ticks()//1000) / 10
-        self.game_sec = (pygame.time.get_ticks()//1000) % 60
