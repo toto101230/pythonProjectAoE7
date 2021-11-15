4f2dbfc59febb36b704cb3ba3023e3ff7316a62b
noemie56140 <noemie56140@hotmail.fr>
2021-11-15 15:27:51 +0100

    Réglage taille HUD

--- hud.py
+++ hud.py
@@ -47,8 +47,9 @@ class Hud:
 
     def create_build_hud(self):
 
-        render_pos = [self.hud_action_rect.x * 1.035, self.hud_action_rect.y * 1.08]
+        render_pos = [self.hud_action_rect.x * (867 * 1.035 / self.hud_action_rect.x), self.hud_action_rect.y * (515 * 1.08 / self.hud_action_rect.y)]
         object_width = self.hud_action_surface.get_width() // 10
+        print(self.hud_action_rect.y)
         self.hud_haut_surface.blit(self.hud_haut, (0, 0))
         self.hud_age_surface.blit(self.hud_age, (0, 0))
         self.hud_action_surface.blit(self.hud_action, (0, 0))
@@ -72,7 +73,7 @@ class Hud:
                 }
             )
 
-            render_pos[0] += image_scale.get_width() + image_scale.get_width() * 0.2
+            render_pos[0] += image_scale.get_width() + 9 * 1280 / self.width
 
         return tiles
 
