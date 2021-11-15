b2a93cbfce078004ee2573474891a0e18bd2000d
elpenax <elpenax@gmail.com>
2021-11-15 15:57:02 +0100

    Deuxieme essai d'implementation de la classe group et de l'affichage du rectangle de sélection

--- group.py
+++ group.py
@@ -6,7 +6,8 @@ blue = (89,152,255)
 class Group:
     def __init__(self):
         self.click_pos = pygame.mouse.get_pos()
-        self.rect_form = pygame.Rect(self.click_pos[0], self.click_pos[1], 0, 0)
+        self.rect_form = None
+        self.nb = 0
 
     def draw(self, screen):
         pygame.event.get()
@@ -15,10 +16,15 @@ class Group:
         pygame.draw.rect(screen, white, self.rect_form, 5)
 
     def update(self):
-        self.click_pos = pygame.mouse.get_pos()
-        self.rect_form = pygame.Rect(self.click_pos[0], self.click_pos[1], 0, 0)
+        if pygame.mouse.get_pressed()[0] and self.nb == 0:
+            self.nb=1
+            self.click_pos = pygame.mouse.get_pos()
+            self.rect_form = pygame.Rect(self.click_pos[0], self.click_pos[1], 0, 0)
+            self.rect_form.update(self.click_pos[0], self.click_pos[1], 0, 0)
+
+        if not pygame.mouse.get_pressed()[0]:
+            self.nb=1
 
         if pygame.mouse.get_pressed()[1]:
             #TODO deselectionner = vider la liste
             pass
-
