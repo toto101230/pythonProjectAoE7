b248816f51d1263d5e17cbd4fe9d0741a49a28ea
elpenax <elpenax@gmail.com>
2021-11-20 10:51:51 +0100

    Lorsque l'on clic and drag avec le bouton gauche, cela trace un rectangle sur l'écran. Au moment ou l'on lache le clic gauche, les coordonnées du rectangle affiché sont stockées dans une variable (self.rec_coord) de la classe Group
    edit : rectification de l'obtention des coordonnées

--- group.py
+++ group.py
@@ -8,7 +8,6 @@ class Group:
     def __init__(self):
         self.init_pos = None
         self.curr_pos = None
-        self.rel_pos = None
         self.rec_coord = None
         self.rect_form = pygame.Rect(0, 0, 0, 0)
         self.nb = 0
@@ -20,13 +19,13 @@ class Group:
         if pygame.mouse.get_pressed()[0]: #clic gauche appuyé
             if self.init_pos is None:
                 self.init_pos = pygame.mouse.get_pos()
-                pygame.mouse.get_rel()
             self.rect_form.update(self.init_pos[0], self.init_pos[1], pygame.mouse.get_pos()[0] - self.init_pos[0],
                                   pygame.mouse.get_pos()[1] - self.init_pos[1])
 
         if not pygame.mouse.get_pressed()[0]:  #clic gauche pas appuyé
             if self.init_pos is not None:
-                self.rec_coord = (self.init_pos[0], self.init_pos[1], pygame.mouse.get_pos()[0] - self.init_pos[0], pygame.mouse.get_pos()[1] - self.init_pos[1])  #coordonées sur l'écran du rectangle tracé
+                self.rec_coord = (self.init_pos[0], self.init_pos[1], pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])  #coordonnées sur l'écran du rectangle tracé
+                print(self.rec_coord)
             self.init_pos = None
 
 
