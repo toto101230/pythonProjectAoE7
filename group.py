d74cd1a5ae473fe49432e7f91a84f4171999b229
elpenax <elpenax@gmail.com>
2021-11-21 10:03:21 +0100

    Tentative d'ajout de villageois à la liste "selected" de la classe groupe en utilisant le rectangle de selection.
    Rectification du calcul pour passer des coordonées du rectangle à plat comme on le voit visuellement aux coordonées réel sur la grille du jeu. (l.16 à 26)
    
    Pour vérifier si un villageois est bien dans la zone délimité par le rectangle tracé (visuellement), mais qui est un parrallélogramme sur la grille, il faut vérifier que l'aire des 4 triangles de sommet = à la position du villageois sur la grille est égale à l'aire du parralélogramme.
    Il faut donc calculer 2 données :
    - l'aire des 4 triangles
    - l'aire du parralélogramme

--- group.py
+++ group.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from selection import Selection
 from settings import TILE_SIZE
 
@@ -7,20 +8,67 @@ class Group:
     def __init__(self):
         self.selected = []
 
-
     def update(self, selection, world, camera):
         if selection.rec_coord is not None:
-            grid_rec_pos = (world.mouse_to_grid(selection.rec_coord[0], selection.rec_coord[1], camera.scroll)[0],
-                            world.mouse_to_grid(selection.rec_coord[0], selection.rec_coord[1], camera.scroll)[1],
-                            world.mouse_to_grid(selection.rec_coord[2], selection.rec_coord[3], camera.scroll)[0],
-                            world.mouse_to_grid(selection.rec_coord[2], selection.rec_coord[3], camera.scroll)[1])
+
+            # var
+
+            # topleft
+            A = (world.mouse_to_grid(selection.rec_coord[0], selection.rec_coord[1], camera.scroll))
+
+            # bottomright
+            B = (world.mouse_to_grid(selection.rec_coord[2], selection.rec_coord[3], camera.scroll))
+
+            # topright
+            C = (world.mouse_to_grid(selection.rec_coord[2], selection.rec_coord[1], camera.scroll))
+
+            # bottomleft
+            D = (world.mouse_to_grid(selection.rec_coord[0], selection.rec_coord[3], camera.scroll))
+
             if self.selected == []:
                 for u in world.unites:
-                        if grid_rec_pos[0] < u.pos[0] < grid_rec_pos[2] and grid_rec_pos[1] < u.pos[1] < grid_rec_pos[3]:
-                                self.selected.append(u)
-
+                    if self.is_in_selection(u,A,B,C,D):
+                        self.selected.append(u)
 
         if pygame.mouse.get_pressed()[2]:  # deselection de toutes les unites
             if self.selected != []:
+                print(self.selected)
                 self.selected.clear()
 
+    def is_in_selection(self, unite, A, B, C, D):
+        t1 = self.tri_area(A,C,unite.pos)
+        t2 = self.tri_area(C,B,unite.pos)
+        t3 = self.tri_area(B,D,unite.pos)
+        t4 = self.tri_area(D,A,unite.pos)
+        if t1+t2+t3+t4 - self.poly_area(A,C,B) < 0 :
+            return True
+        return False
+
+    def poly_area(self, A, C, D):
+        return 2*self.tri_area(A,C,D)
+
+    def tri_area(self, A, B ,C):
+        p = (self.tri_perimeter(A,B,C))/2
+        return self.isqrt_dicho_rec(p*(p-self.segment_len(A,B))*(p-self.segment_len(B,C))*(p-self.segment_len(C,A))) #formule de Héron
+
+    def tri_perimeter(self, A, B, C):
+        return self.segment_len(A,B) + self.segment_len(B,C) + self.segment_len(C,A)
+
+    def segment_len(self, A, B):
+        return self.isqrt_dicho_rec((B[0]-A[0])**2 + (B[1]-A[1])**2)
+
+    def isqrt_dicho_rec(self, n):
+        low = 0
+        high = n
+
+        def borne(low, high):
+            if high - low > 1:
+                middle = (low + high) // 2
+                if middle * middle > n:
+                    return borne(low, middle)
+                else:
+                    return borne(middle, high)
+            # print(low,high)
+            return high if high * high <= n else low
+
+        return borne(low, high)
