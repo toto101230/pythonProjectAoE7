41cdecfb5649ad02e49214394f7324cb22e30164
noemie56140 <noemie56140@hotmail.fr>
2021-12-20 17:39:33 +0100

    Réglage HUD

--- hud.py
+++ hud.py
@@ -52,10 +52,11 @@ class Hud:
 
     def create_build_hud(self):
 
-        render_pos = [self.hud_action_rect.x + 30 * 1280 / self.width, self.hud_action_rect.y + 40 * 1280 / self.width]
+        render_pos = [self.hud_action_rect.x + 30, self.hud_action_rect.y + 40 ]
+        #* 1280 / self.width
         # (867 * 1.035 / self.hud_action_rect.x)
         # (515 * 1.08 / self.hud_action_rect.y)
-        object_width = self.hud_action_surface.get_width() // (10 * self.width / 1280)
+        object_width = self.hud_action_surface.get_width() // 15
         self.hud_haut_surface.blit(self.hud_haut, (0, 0))
         self.hud_age_surface.blit(self.hud_age, (0, 0))
         self.hud_action_surface.blit(self.hud_action, (0, 0))
