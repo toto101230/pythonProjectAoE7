bb7b6b7babde8c21d6ee33c22446622ece5b9e57
Thomas <tpienne51@gmail.com>
2022-01-12 09:18:53 +0100

    La caméra est centrée sur l'HDV

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import pygame
+from settings import TILE_SIZE
 
 
 class Camera:
@@ -7,28 +8,42 @@ class Camera:
         self.width = width
         self.height = height
 
-        self.scroll = pygame.Vector2(-5000, -400)
+        self.scroll = pygame.Vector2(0, 0)
         self.dx = 0
         self.dy = 0
         self.speed = 20
 
-        self.yBoolM, self.yBoolP, self.xBoolM, self.xBoolP, = False, False, False, False,
+        self.yBoolM, self.yBoolP, self.xBoolM, self.xBoolP, = False, False, False, False
+
+    def pos_hdv_base(self, pos):
+        cart_x = (pos[0] -TILE_SIZE//5) * TILE_SIZE
+        cart_y = (pos[1] - 1) * TILE_SIZE
+
+        world_x = cart_x - cart_y
+        world_y = (cart_y * 2 + world_x) / 2
+
+        self.scroll.x -= world_x + 100 * TILE_SIZE
+        self.scroll.y -= world_y
+
+        # return x, y
 
     def update(self):
         mouse_pos = pygame.mouse.get_pos()
 
-        if mouse_pos[0] > self.width * 0.97:
-            self.dx = -self.speed
-        elif mouse_pos[0] < self.width * 0.03:
-            self.dx = self.speed
-        elif not self.yBoolM and not self.yBoolP and not self.xBoolM and not self.xBoolP:
+        # if mouse_pos[0] > self.width * 0.97:
+        #     self.dx = -self.speed
+        # elif mouse_pos[0] < self.width * 0.03:
+        #     self.dx = self.speed
+        # el
+        if not self.yBoolM and not self.yBoolP and not self.xBoolM and not self.xBoolP:
             self.dx = 0
 
-        if mouse_pos[1] > self.height * 0.97:
-            self.dy = -self.speed
-        elif mouse_pos[1] < self.height * 0.03:
-            self.dy = self.speed
-        elif not self.yBoolM and not self.yBoolP and not self.xBoolM and not self.xBoolP:
+        # if mouse_pos[1] > self.height * 0.97:
+        #     self.dy = -self.speed
+        # elif mouse_pos[1] < self.height * 0.03:
+        #     self.dy = self.speed
+        # el
+        if not self.yBoolM and not self.yBoolP and not self.xBoolM and not self.xBoolP:
             self.dy = 0
 
         self.scroll.x += self.dx
