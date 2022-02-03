51d446b2a9c23bd609e8776002823858380390cc
Mathieu PENE <mathieu.pene@gmail.com>
2022-02-03 19:53:10 +0100

    age fini

--- world.py
+++ world.py
@@ -731,6 +731,9 @@ class World:
             for x in range(0, self.grid_length_x):
                 for y in range(0, self.grid_length_y):
                     building = self.buildings[x][y]
+                    if isinstance(building, Hdv) and building.joueur == joueur and building.construit:
+                        building.health += 50
+                        building.max_health = 700
                     if isinstance(building, Caserne) and building.joueur == joueur and building.construit:
                         building.health += 150
                         building.max_health = 500
@@ -762,6 +765,9 @@ class World:
             for x in range(0, self.grid_length_x):
                 for y in range(0, self.grid_length_y):
                     building = self.buildings[x][y]
+                    if isinstance(building, Hdv) and building.joueur == joueur and building.construit:
+                        building.health += 100
+                        building.max_health = 1100
                     if isinstance(building, Caserne) and building.joueur == joueur and building.construit:
                         building.health += 100
                         building.max_health = 600
